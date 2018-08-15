6376cce5b18b17414d3faec05c1e8a49af170ee1
unknown <lesliealejandrarb@gmail.com>
2018-08-15 13:33:42 -0500

    Agrega y modifica modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "../../../node_modules/@angular/core";
+import { BreadcrumbsComponent } from "./breadcrumbs/breadcrumbs.component";
+import { HeaderComponent } from "./header/header.component";
+import { NopagesfoundComponent } from "./nopagesfound/nopagesfound.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+
+@NgModule({
+    declarations: [
+        BreadcrumbsComponent,
+        HeaderComponent,
+        NopagesfoundComponent,
+        SidebarComponent
+    ],
+    exports:[
+        BreadcrumbsComponent,
+        HeaderComponent,
+        NopagesfoundComponent,
+        SidebarComponent
+    ]
+})
+
+export class SharedModule{}
