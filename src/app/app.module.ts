03687639f95e9b02905d77ac9751b6728740791e
unknown <lesliealejandrarb@gmail.com>
2018-08-15 13:27:10 -0500

    Modificacion de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,11 @@ import { NgModule } from '@angular/core';
 //rutas
 import { APP_ROUTES } from './app.routes';
 
+//modulos
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NopagesfoundComponent } from './shared/nopagesfound/nopagesfound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './login/login.component'
 import { RegisterComponent } from './login/register.component';
 
 
@@ -21,19 +16,13 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagesfoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
+    
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
